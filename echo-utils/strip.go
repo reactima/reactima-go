6332f67826425ae5d0f8cfe4ed375cd354464b06
reactima <1@ilya1.com>
2020-06-17 16:52:18 +0300

    regex fix

--- echo-utils/strip.go
+++ echo-utils/strip.go
@@ -6,19 +6,19 @@ import (
 	"strings"
 )
 
-func StripForCache (s string) string {
+func StripForCache(s string) string {
 	s = strings.TrimSpace(s)
 	s = strings.ToLower(s)
-	s = strings.ReplaceAll(s," ","")
+	s = strings.ReplaceAll(s, " ", "")
 	return s
 }
 
-func RemoveNonAlphanumeric (s string) string {
+func RemoveNonAlphanumeric(s string) string {
 	s = strings.TrimSpace(s)
 	s = strings.ToLower(s)
-	s = strings.ReplaceAll(s," ","")
+	s = strings.ReplaceAll(s, " ", "")
 
-	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	reg, err := regexp.Compile(`[^a-zA-Z0-9]+`)
 	if err != nil {
 		panic(err)
 	}
@@ -28,21 +28,21 @@ func RemoveNonAlphanumeric (s string) string {
 	return s
 }
 
-func StripSubdomainPlus (s string) string {
+func StripSubdomainPlus(s string) string {
 
 	u, err := url.Parse(s)
 	if err != nil {
 		return s
 	}
 	parts := strings.Split(u.Hostname(), ".")
-	if len(parts)>2 {
-		if parts[0]!="" {
-			s = strings.ReplaceAll(s,parts[0]+".","")
+	if len(parts) > 2 {
+		if parts[0] != "" {
+			s = strings.ReplaceAll(s, parts[0]+".", "")
 		}
 	}
 
-	s = strings.ReplaceAll(s,"http://","")
-	s = strings.ReplaceAll(s,"https://","")
+	s = strings.ReplaceAll(s, "http://", "")
+	s = strings.ReplaceAll(s, "https://", "")
 
 	s = strings.TrimSpace(s)
 	s = strings.ToLower(s)
